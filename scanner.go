d64bb4fa5a462cc022663366721cff73f56feddf
Daniil Stepanov <132468035+c0mrade12211@users.noreply.github.com>
2024-02-19 03:01:12 +0300

    Update scanner.go

--- scanner.go
+++ scanner.go
@@ -8,20 +8,30 @@ import (
 
 func TCPScanner(network, address string, numsOfPort int) {
 	var wg sync.WaitGroup
+	semaphore := make(chan struct{}, 50) 
 
 	for i := 0; i <= numsOfPort; i++ {
+		semaphore <- struct{}{} 
 		wg.Add(1)
 		go func(j int) {
-			defer wg.Done()
-			conn, err := net.Dial(network, fmt.Sprintf(address+":%d", j))
+			defer func() {
+				<-semaphore 
+				wg.Done()
+			}()
+
+			conn, err := net.DialTimeout(network, fmt.Sprintf("%s:%d", address, j))
 			if err != nil {
-				fmt.Println("Port is closed", j)
+				fmt.Println("[-] Port Closed", j)
 				return
 			}
-			fmt.Println("Connection successful on port ", j)
-			conn.Close()
-
+			defer conn.Close()
+			fmt.Println("[+] Port open", j)
 		}(i)
 	}
+
 	wg.Wait()
+}
+
+func main() {
+	TCPScanner("tcp", "127.0.0.1", 100)
 }
